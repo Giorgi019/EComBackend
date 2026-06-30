1cfaa57a01a9b3e2e5c712086b304e09d972d039
Giorgi019 <dekanoidze07@gmail.com>
2026-06-30 18:48:02 +0400

    Add findAll method in ProductService

---
+++ src/main/java/com/giorgi/EComBackend/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.giorgi.EComBackend.service;
+
+import com.giorgi.EComBackend.model.Product;
+import com.giorgi.EComBackend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
