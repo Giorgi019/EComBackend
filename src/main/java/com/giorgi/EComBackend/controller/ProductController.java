e1bfa6312424424f67d49c1a328ba1f87e4a352f
Giorgi019 <dekanoidze07@gmail.com>
2026-06-30 19:10:19 +0400

    Add createProduct POST endpoint in ProductService

--- src/main/java/com/giorgi/EComBackend/controller/ProductController.java
+++ src/main/java/com/giorgi/EComBackend/controller/ProductController.java
@@ -6,9 +6,7 @@ import com.giorgi.EComBackend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,11 @@ public class ProductController {
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = productService.findAll();
         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product savedProduct = productService.saveProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
     }
 
 }
